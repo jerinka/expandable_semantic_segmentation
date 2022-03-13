51aaf178ece3d3ce19b016be0420ce3f04c0fd94
jerin <jerin>
2022-03-13 23:34:40 +0530

    expandable semantic segmentation

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # expandable_semantic_segmentation
 Training using crops, inferring on large image
+
+# Label images
+Install steps: https://github.com/wkentaro/labelme \
+```cd dataset/train``` and then repeat steps for val\
+```labelme --nodata --autosave --labels cats```\
+
+# Json to Mask
+Convert json to mask via,\
+```python json2mask.py```
+
+# Semantic segmentation
+```jupyter notebook```\
+```semanitc_seg.ipynb```
+
+
+
---
+++ json2mask.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import argparse
+import json
+import os
+import os.path as osp
+import base64
+import warnings
+
+import PIL.Image
+import yaml
+
+from labelme import utils
+
+import cv2
+import numpy as np
+from skimage import img_as_ubyte
+
+# from sys import argv
+
+def json2mask(json_file='dataset/train',outpath='dataset/dataXY/train', outHW=(256,256)):
+ 
+    cv2.namedWindow(winname='img', flags=cv2.WINDOW_NORMAL)
+    cv2.namedWindow(winname='mask', flags=cv2.WINDOW_NORMAL)
+
+    list_path = [f for f in os.listdir(json_file) if f.endswith('.json')]
+    if len(list_path):
+        outpathimg=osp.join(outpath,'img')
+        outpathmask=osp.join(outpath,'mask')
+        os.makedirs(name=outpathimg, exist_ok=True)
+        os.makedirs(name=outpathmask, exist_ok=True)
+    labels = []
+    for i in range(0,len(list_path)):
+        path = os.path.join(json_file,list_path[i])
+        if os.path.isfile(path):
+            data = json.load(open(path))
+        img_path = os.path.join(json_file,list_path[i].replace('.json','.jpg'))
+        img = cv2.imread(img_path)
+        mask  = np.zeros(img.shape[:2], dtype=np.uint8)
+        
+        #import pdb;pdb.set_trace()
+
+        for j in range(len(data['shapes'])):
+            points = data['shapes'][j]['points']
+            label = data['shapes'][j]['label']
+            if label not in labels:
+                labels.append(label)
+            ind = labels.index(label)
+            #Draw filled polygon
+            cv2.fillPoly(mask, np.array([points], dtype=np.int32),color=ind+1, lineType=cv2.LINE_8)
+        img = cv2.resize(img, dsize=outHW)
+        mask = cv2.resize(mask, dsize=outHW)
+
+        cv2.imwrite(os.path.join(outpathimg,list_path[i].replace('.json','.png')),img)
+        cv2.imwrite(os.path.join(outpathmask,list_path[i].replace('.json','.png')),mask)
+        cv2.imshow('img', img)
+        cv2.imshow('mask', mask*64)
+        k=cv2.waitKey(100)
+        if k==27:
+            break
+
+if __name__ == '__main__':
+    #base64path = argv[1]
+    json2mask(json_file='dataset/train',outpath='dataset/dataXY/train', outHW=(128,128))
+    json2mask(json_file='dataset/val',outpath='dataset/dataXY/val', outHW=(128,128))
